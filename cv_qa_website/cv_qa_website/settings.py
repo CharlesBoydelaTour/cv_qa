4df057bd8aded5d011b04b9e611e7c28e9651ce8
Charles Boy-de-la-tour <charles.boy-de-la-tour-consultant@dgfip.finances.gouv.fr>
2023-10-31 12:33:39 +0100

    commit

--- cv_qa_website/cv_qa_website/settings.py
+++ cv_qa_website/cv_qa_website/settings.py
@@ -24,7 +24,6 @@ load_dotenv()
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-
 CSRF_TRUSTED_ORIGINS = ["https://webappcvqa.azurewebsites.net", "https://*.127.0.0.1"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
